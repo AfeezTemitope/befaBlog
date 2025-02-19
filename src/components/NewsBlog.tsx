2b96ed4a1bec4a4c7c7bc1339609b8aad5f93f00
AfeezTemitope <belloafeez28@gmail.com>
2025-02-19 11:45:59 +0100

    added the admin, training scheduled and added player of the month too

--- src/components/NewsBlog.tsx
+++ src/components/NewsBlog.tsx
@@ -3,9 +3,7 @@ const NewsBlog = () => {
         <div className="py-10 sm:py-6 lg:py-8 ">
             <h2 className="text-3xl font-bold text-center mb-8"> SPORTS NEWS </h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4 sm:gap-6 px-4">
-                {
-
-                }
+                <a>budu elites</a>
             </div>
         </div>
 
