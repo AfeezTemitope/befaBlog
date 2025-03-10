cadc87adb7c16a8dd2445a2c1f27a5708c7b15d7
AfeezTemitope <belloafeez28@gmail.com>
2025-03-10 13:48:56 +0100

    addd workflow

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,47 +1,3 @@
-// import React from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import Befa from '../assets/Befa.png';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faSearch, faUserCircle } from '@fortawesome/free-solid-svg-icons';
-//
-// const Header: React.FC = () => {
-//     const navigate = useNavigate();
-//
-//     const goToHome = () => {
-//         navigate('/');
-//     };
-//
-//     const goToAdminPage =()=>{
-//         navigate('/admin')
-//     }
-//
-//     return (
-//         <header className="bg-black text-white py-4 px-4 md:px-8 flex md:flex-row justify-between items-center border-b border-l-emerald-400">
-//             <div className="mb-4 md:mb-0 cursor-pointer" onClick={goToHome}>
-//                 <img src={Befa} alt="Befa Logo" className="h-12 w-auto md:h-16" />
-//             </div>
-//             <div className="flex flex-col items-center text-center mb-4 md:mb-0">
-//                 <span className="text-lg font-bold" onClick={goToAdminPage}>BEFA</span>
-//                 <span className="text-sm">budu elite football academy
-//                 </span>
-//             </div>
-//             <div className="flex items-center space-x-4">
-//                 <FontAwesomeIcon
-//                     icon={faUserCircle}
-//                     size="lg"
-//                     className="hover:text-gray-400 cursor-pointer"
-//                 />
-//                 <FontAwesomeIcon
-//                     icon={faSearch}
-//                     size="lg"
-//                     className="hover:text-gray-400"
-//                 />
-//             </div>
-//         </header>
-//     );
-// };
-//
-// export default Header;
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Befa from '../assets/Befa.png';
@@ -63,8 +19,8 @@ const Header: React.FC = () => {
                 <img src={Befa} alt="Befa Logo" className="h-10 w-auto md:h-16 mr-4" />
             </div>
             <div className="flex flex-col items-center flex-grow">
-                <span className="text-2xl font-extrabold cursor-pointer" onClick={goToAdminPage}>BEFA</span>
-                <span className="text-xs">budu elite football academy</span>
+                <span className="text-4xl font-extrabold cursor-pointer">BEFA</span>
+                <span className="text-xs"  onClick={goToAdminPage}>budu elite football academy</span>
             </div>
         </header>
     );
